75bcdade5887d6385464f43ec4a2e54f560e735c
kyle <kyle@kylelee.dev>
2024-10-29 18:38:58 -0400

    add dockerfile

--- auction-service/hello.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-
-def main():
-    print("Hello from auction-service!")
-
-
-if __name__ == "__main__":
-    main()
---
+++ auction-service/main.py
@@ -0,0 +1,28 @@
+from typing import Union
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+@app.get("/checkhealth")
+async def handle_checkhealth():
+    return "API is up!"
+
+
+def main():
+    print("Hello from auction-service!")
+
+
+if __name__ == "__main__":
+    main()
