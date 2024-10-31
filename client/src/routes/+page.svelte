7bd0517732046249ed8b74318dcbc07f4f522aa0
kyle <kyle@kylelee.dev>
2024-10-31 16:55:23 -0400

    add test client

---
+++ client/src/routes/+page.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	let auctions = $state([]);
+
+	$effect(() => {
+		async function getAuctions() {
+			const url = 'http://localhost:8000/api/auction/';
+			const res = await fetch(url);
+			const data = await res.json();
+			auctions = data;
+		}
+		getAuctions();
+	});
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<ul>
+	<!-- {#each auctions as auction (user.age)} -->
+	<!-- 	<li> -->
+	<!-- 		name: {user.name} -->
+	<!-- 	</li> -->
+	<!-- 	<li> -->
+	<!-- 		age: {user.age} -->
+	<!-- 	</li> -->
+	<!-- 	<li> -->
+	<!-- 		favorite food: {user.favorite_food} -->
+	<!-- 	</li> -->
+	<!-- {/each} -->
+</ul>
+
+<style>
+</style>
