4f78aeb62f695a982bd85ff3126ed095cb3f7272
kyle <kyle@kylelee.dev>
2024-11-01 16:10:22 -0400

    add basic functionality for category routes

--- auction-service/models/category.py
+++ auction-service/models/category.py
@@ -6,6 +6,18 @@ from pydantic import BaseModel, Field, StringConstraints
 from datetime import datetime
 
 
+class CategoryRequest(BaseModel):
+    name: Annotated[str, StringConstraints(max_length=50)]
+    description: Optional[str] = None
+    created_at: datetime = Field(default_factory=datetime.now)
+    updated_at: datetime = Field(default_factory=datetime.now)
+
+    class Config:
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
+        from_attributes = True  # Use from_attributes instead of orm_mode
+
+
 class CategoryModel(BaseModel):
     id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
     name: Annotated[str, StringConstraints(max_length=50)]
@@ -17,3 +29,9 @@ class CategoryModel(BaseModel):
         arbitrary_types_allowed = True
         json_encoders = {ObjectId: str}
         from_attributes = True  # Use from_attributes instead of orm_mode
+
+
+# Helper function to convert MongoDB ObjectId to string
+def category_helper(category) -> dict:
+    category["_id"] = str(category["_id"])
+    return category
--- auction-service/routes/category.py
+++ auction-service/routes/category.py
@@ -1,6 +1,8 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from pymongo.collection import Collection
+from typing import List
 from mongodb import get_db
+from models.category import CategoryModel, CategoryRequest, category_helper
 
 router = APIRouter(
     prefix="/category",
@@ -15,24 +17,40 @@ router = APIRouter(
 @router.get("/", tags=["category"])
 async def get_category(db: Collection = Depends(get_db)):
     category_collection = db["categories"]
-    category_collection.find({})
+    categories = category_collection.find({})
 
-    return {"status": 200}
+    return [category_helper(doc) for doc in categories]
 
 
 # create a new category
+# TODO: should probably take a list as input
 @router.post("/", tags=["category"])
-async def create_category():
-    return {"status": 200}
+async def create_category(
+    category_list: List[CategoryRequest], db: Collection = Depends(get_db)
+):
+    category_collection = db["categories"]
+    maybe_new_categories = category_collection.insert_many(
+        [category for category in category_list]
+    )
+    if maybe_new_categories.acknowledged:
+        return maybe_new_categories.inserted_ids
+
+    raise HTTPException(status_code=409, detail=f"Could not create categories")
 
 
 # update a category
-@router.put("/", tags=["category"])
-async def update_category():
+@router.put("/{category_id}", tags=["category"])
+async def update_category(category_id: str, category, db: Collection = Depends(get_db)):
+
+    category_collection = db["categories"]
+    category_collection.update_one({"_id": category_id}, dict(category))
     return {"status": 200}
 
 
 # delete a category
 @router.delete("/{category_id}", tags=["category"])
-async def delete_category(auction_id: int):
-    return {"status": 200}
+async def delete_category(category_id: str, db: Collection = Depends(get_db)):
+
+    category_collection = db["categories"]
+    maybe_deleted_category = category_collection.delete_one({"_id": category_id})
+    return maybe_deleted_category.acknowledged
