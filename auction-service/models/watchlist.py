d052901a39e6bd73d742e081b918891026a5e05b
kyle <kyle@kylelee.dev>
2024-11-01 16:14:54 -0400

    add watchlist modules

---
+++ auction-service/models/watchlist.py
@@ -0,0 +1,9 @@
+from models.objectid import PyObjectId
+from pydantic import BaseModel, Field, StringConstraints
+from decimal import Decimal
+from typing import List, Optional
+from typing_extensions import Annotated
+from datetime import datetime
+from bson import ObjectId
+
+# TODO: Create watchlist models
