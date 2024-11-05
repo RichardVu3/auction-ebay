21e51a53644e971cce73561ffc21adf8106e3b81
kyle <kyle@kylelee.dev>
2024-11-04 21:03:14 -0500

    remove dead code

--- client/src/components/GlobalNavbar.jsx
+++ client/src/components/GlobalNavbar.jsx
@@ -26,12 +26,6 @@ const menuItems = [
   },
 ];
 
-//used for responsive menu items
-const activeClass =
-  "rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white";
-const defaultClass =
-  "rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white";
-
 const MobileMenuHamburgerButton = () => {
   return (
     <>
@@ -137,93 +131,17 @@ const MobileMenuPanel = () => {
             </DisclosureButton>
           );
         })}
-
-        {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
-        {/*
-        <DisclosureButton
-          as={NavLink}
-          href="#"
-          className="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white"
-        >
-          Dashboard
-        </DisclosureButton>
-        <DisclosureButton
-          as="a"
-          href="#"
-          className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-        >
-          Team
-        </DisclosureButton>
-        <DisclosureButton
-          as="a"
-          href="#"
-          className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-        >
-          Projects
-        </DisclosureButton>
-        <DisclosureButton
-          as="a"
-          href="#"
-          className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-        >
-          Calendar
-        </DisclosureButton>
-      </div>
-      <div className="border-t border-gray-700 pb-3 pt-4">
-        <div className="flex items-center px-5">
-          <div className="flex-shrink-0">
-            <img
-              alt=""
-              src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-              className="h-10 w-10 rounded-full"
-            />
-          </div>
-          <div className="ml-3">
-            <div className="text-base font-medium text-white">Tom Cook</div>
-            <div className="text-sm font-medium text-gray-400">
-              tom@example.com
-            </div>
-          </div>
-          <button
-            type="button"
-            className="relative ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-          >
-            <span className="absolute -inset-1.5" />
-            <span className="sr-only">View notifications</span>
-            <BellIcon aria-hidden="true" className="h-6 w-6" />
-          </button>
-        </div>
-        <div className="mt-3 space-y-1 px-2">
-          <DisclosureButton
-            as="a"
-            href="#"
-            className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
-          >
-            Your Profile
-          </DisclosureButton>
-          <DisclosureButton
-            as="a"
-            href="#"
-            className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
-          >
-            Settings
-          </DisclosureButton>
-          <DisclosureButton
-            as="a"
-            href="#"
-            className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
-          >
-            Sign out
-          </DisclosureButton>
-        </div>
-
-		  */}
       </div>
     </DisclosurePanel>
   );
 };
 
 export default function GlobalNavbar() {
+  //used for responsive menu items
+  const activeClass =
+    "rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white";
+  const defaultClass =
+    "rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white";
   return (
     <Disclosure as="nav" className="bg-gray-800">
       <div className="mx-auto max-w-7xl px-2 sm:px-4 lg:px-8">
@@ -252,34 +170,6 @@ export default function GlobalNavbar() {
                     </NavLink>
                   );
                 })}
-                {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
-
-                {/*map over the list of links here*/}
-                {/*<a
-                  href="#"
-                  className="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white"
-                >
-                  Dashboard
-                </a>
-                <a
-                  href="#"
-                  className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-                >
-                  Team
-                </a>
-                <a
-                  href="#"
-                  className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-                >
-                  Projects
-                </a>
-                <a
-                  href="#"
-                  className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-                >
-                  Calendar
-                </a>
-				*/}
               </div>
             </div>
           </div>
