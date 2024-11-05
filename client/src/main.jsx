59203fdf17f961359e0b21482fb247e88d272b49
kyle <kyle@kylelee.dev>
2024-11-05 18:06:05 -0500

    add react query

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,9 +7,13 @@ import {
   Route,
   RouterProvider,
 } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 import App from "./app";
 import ErrorPage from "./error-page";
 import Index from "./routes/index";
+
+const queryClient = new QueryClient();
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -25,6 +29,8 @@ const router = createBrowserRouter(
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </StrictMode>,
 );
