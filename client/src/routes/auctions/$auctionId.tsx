5ae978a30f6aa59211e1c7be988c8e5bf9af9749
Kyle Lee <kyle@kylelee.dev>
2024-11-12 11:19:22 -0500

    add single auction view

--- client/src/routes/auctions/$auctionId.tsx
+++ client/src/routes/auctions/$auctionId.tsx
@@ -1,6 +1,7 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { auctionQueryOptions } from "../../utils/queryOptions";
+import { Card, CardContent } from "@/components/ui/card";
 import {
   Carousel,
   CarouselContent,
@@ -20,11 +21,71 @@ export const Route = createFileRoute("/auctions/$auctionId")({
   component: RouteComponent,
 });
 
+export function CarouselOrientation() {
+  return (
+    <Carousel
+      opts={{
+        align: "start",
+      }}
+      orientation="vertical"
+      className="w-full max-w-xs"
+    >
+      <CarouselContent className="-mt-1 h-[200px]">
+        {Array.from({ length: 5 }).map((_, index) => (
+          <CarouselItem key={index} className="pt-1 md:basis-1/2">
+            <div className="p-1">
+              <Card>
+                <CardContent className="flex items-center justify-center p-6">
+                  <span className="text-3xl font-semibold">{index + 1}</span>
+                </CardContent>
+              </Card>
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  );
+}
+
+export function CarouselDemo() {
+  return (
+    <Carousel className="w-full">
+      <CarouselContent>
+        {Array.from({ length: 5 }).map((_, index) => (
+          <CarouselItem key={index}>
+            <div className="p-1">
+              <Card>
+                <CardContent className="flex aspect-square items-center justify-center p-6">
+                  <span className="text-4xl font-semibold">{index + 1}</span>
+                </CardContent>
+              </Card>
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  );
+}
+
 function RouteComponent() {
   const params = Route.useParams();
   const auctionQuery = useSuspenseQuery(auctionQueryOptions(params.auctionId));
   const auction = auctionQuery.data[0];
   console.log(auction, "???");
 
-  return <div className="text-white">{auction.title}</div>;
+  return (
+    <div className="flex flex-wrap">
+      <div className="flex-none">
+        <CarouselDemo />
+      </div>
+      <div className="flex-grow">
+        <h1>{auction.title}</h1>
+        <p>{auction.description}</p>
+      </div>
+    </div>
+  );
 }
