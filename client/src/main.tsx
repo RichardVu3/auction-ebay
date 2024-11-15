022e86108ebac8bc381977eae462e9cb751c9794
Kyle Lee <kyle@kylelee.dev>
2024-11-15 00:16:57 -0500

    add amplify integration

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -11,7 +11,7 @@ export const queryClient = new QueryClient();
 
 import { Authenticator } from "@aws-amplify/ui-react";
 import { Amplify } from "aws-amplify";
-import outputs from "@/amplify_outputs.json";
+import outputs from "../amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
 
 Amplify.configure(outputs);
